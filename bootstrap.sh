539912baeec0570d4c6462f91d203ceec71e7459
sleepingcat <sleepingcat@lonely-desk.top>
2024-04-15 10:40:02 +0100

    chore: remove infisical stuff, add pip requirements

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+#
+# Get uv and install a prefix with ansible on it
+
+yay -S --needed --noconfirm uv
+git clone /repos/littlelab.git && cd littlelab
+uv venv .ansible_venv && source .ansible_venv/bin/activate
+uv pip install requirements.txt
