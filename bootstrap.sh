2dfc0d252786ed4bd32f9ba2c621cacac5cde150
sleepingcat <sleepingcat@lonely-desk.top>
2024-05-18 19:48:55 +0100

    feat & chore: journald role, memory limits, bump

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-#
-# Get uv and install a prefix with ansible on it
-
-yay -S --needed --noconfirm uv rbw expect
-git clone /repos/littlelab.git && cd littlelab
-uv venv .ansible_venv && source .ansible_venv/bin/activate
-uv pip install requirements.txt
